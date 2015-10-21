15a106225cc4cb39e7f60dd98a95ccb17002186e
Almir <almir.b@aquila-ars.com>
2015-10-21 11:39:00 +0200

    Event listener for changing the search input

--- background.js
+++ background.js
@@ -1,7 +1,18 @@
-chrome.extension.onRequest.addListener(
-    function (request, sender, callback) {
 
-    }
-);
 
-console.log('Hi!');
+function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+        results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+var input = document.querySelector('input#lst-ib');
+
+input.addEventListener('input', function()
+{
+    console.log(input.value);
+});
+
+console.log(getParameterByName('q'));
+
